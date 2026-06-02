63074bbabf08cb3cd15389d68df6aac8a71a5e5a
Trycilly <dominic.seh@gmail.com>
2026-06-02 12:51:26 +0200

    isk

---
+++ lab2a/build/CMakeFiles/4.3.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/domin/Desktop/Computerarchitektur/computerarchitektur_arm/external/pico-sdk/cmake/preload/toolchains/pico_riscv_gcc.cmake")
+
+set(CMAKE_SYSTEM "PICO")
+set(CMAKE_SYSTEM_NAME "PICO")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "hazard3")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
